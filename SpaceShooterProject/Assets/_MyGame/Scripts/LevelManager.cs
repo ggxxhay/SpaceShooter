918a1b1b48fd52fdde4feba1d869508cf3313571
THANG-HR-TT\ggxxh <ggxxhay123@gmail.com>
2018-05-03 17:39:19 +0700

    Menu and Slider

---
+++ SpaceShooterProject/Assets/_MyGame/Scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	// Change Scene
+    public void ChangeLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // Exit Application
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
