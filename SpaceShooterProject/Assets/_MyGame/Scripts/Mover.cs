8fe2900673b2d6a0efc08d07554ffc08302d35eb
THANG-HR-TT\ggxxh <ggxxhay123@gmail.com>
2018-05-04 16:09:07 +0700

    Enemy move and fire

---
+++ SpaceShooterProject/Assets/_MyGame/Scripts/Mover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    public float speed;
+
+	// Move the object
+    private void Start()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+    }
+}
