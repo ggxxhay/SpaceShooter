5a1e9744ea150a6a3c47af6826625b82b7461bdc
THANG-HR-TT\ggxxh <ggxxhay123@gmail.com>
2018-05-08 13:58:52 +0700

    Score

---
+++ SpaceShooterProject/Assets/_MyGame/Scripts/Score.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    private int score;
+
+    public Text scoreUI;
+
+    // Use this for initialization
+    void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreUI.text = "Score: " + score;
+    }
+
+    public void AddPoint(int point)
+    {
+        score += point;
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+}
